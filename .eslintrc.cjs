0ab783ed7dbcdbf29ca05832df630912c776ce3b
anyesu <iewgggg@qq.com>
2023-09-23 14:37:05 +0800

    build: :wrench: setup development environment configurations
    - support eslint-plugin-unicorn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,21 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    '@vue/prettier',
     '@vue/typescript/recommended',
     'plugin:n/recommended',
     'plugin:regexp/recommended',
+    'plugin:unicorn/recommended',
+    // Make sure to put prettier last, so it gets the chance to override other configs.
+    '@vue/prettier',
   ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
   rules: {
     'n/no-missing-import': 0,
+    'unicorn/prevent-abbreviations': 0,
+    'unicorn/filename-case': 0,
+    'regexp/no-super-linear-backtracking': 0,
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-empty-interface': 0,
     '@typescript-eslint/no-explicit-any': 0,
