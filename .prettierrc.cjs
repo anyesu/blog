928db442fe846a754ebd15b7a4260e9d5336fe1d
anyesu <iewgggg@qq.com>
2023-11-16 00:31:41 +0800

    fix: :bug: output like `JSON.stringify` by `json-stringify`

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -22,6 +22,7 @@ module.exports = {
     {
       files: 'docs/**/*.config.json',
       options: {
+        parser: 'json-stringify',
         plugins: ['prettier-plugin-sort-json'],
       },
     },
